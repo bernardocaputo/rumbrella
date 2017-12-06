9af93a6889c3b24a7a333d3be94157098356b6e8
Bernardo Caputo <bernardocaputo@gmail.com>
2017-12-06 15:00:52 -0200

    umbrella chapter

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Rumbrella.Mixfile do
     [
       apps_path: "apps",
       start_permanent: Mix.env == :prod,
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       deps: deps()
     ]
   end
@@ -14,7 +16,7 @@ defmodule Rumbrella.Mixfile do
   # the apps folder.
   #
   # Run "mix help deps" for examples and options.
-  defp deps do
-    []
+  def deps do
+    [{:sweet_xml, "~> 0.5.0"}]
   end
 end
